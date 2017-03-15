58f9fde8afcf6fd69706a1d05b666402aab91589
Kyrie <424629123@qq.com>
2017-03-15 17:34:01 +0800

    添加webapi项目

---
+++ MyFrameWork.WebAPI/Controllers/StudentController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MyFrameWork.Models.Models;
+using MyFrameWork.Services.Interface;
+
+namespace MyFrameWork.WebAPI.Controllers
+{
+    public class StudentController : Controller
+    {
+        //
+        // GET: /Student/
+
+        public IStudentService StudentService { set; get; }  
+        
+        public ActionResult Index()
+        {
+            Student student = StudentService.FindByOptons(s => s.StudentID == 1).FirstOrDefault() ;
+
+            return Json("sdfa",JsonRequestBehavior.AllowGet);
+        }
+
+    }
+}
---
+++ MyFrameWork.WebAPI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MyFrameWork.WebAPI.WebApiApplication" Language="C#" %>
